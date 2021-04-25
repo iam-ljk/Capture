8d1335d5adaf4762e92abbf2970a225c5fd9bdcc
iam-ljk <jay4luhar@gmail.com>
2021-04-25 20:36:21 +0530

    MovieDetails start

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-import React from 'react';
-//Global Style 
-import GlobalStyle from './components/GlobalStyle'
+import React from "react";
+//Global Style
+import GlobalStyle from "./components/GlobalStyle";
 //import pages
-import AboutUs from './pages/AboutUs'
-import Nav from './components/Nav'
+import AboutUs from "./pages/AboutUs";
+import Nav from "./components/Nav";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
+import MovieDetail from './pages/MovieDetail'
+//Router
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work" exact>
+          <OurWork />
+        </Route>
+        <Route path="/work/:id">
+          <MovieDetail />
+        </Route>
+        <Route path="/contact" exact>
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
